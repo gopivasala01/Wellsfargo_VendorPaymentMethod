95c33e93573ab900ccc19dcae0e81eab8258dd60
Gopi V <gopi@lp.local>
2024-02-12 15:58:33 +0530

    App config market added Georgia

--- src/main/java/mainPackage/RunnerClass.java
+++ src/main/java/mainPackage/RunnerClass.java
@@ -37,7 +37,7 @@ public class RunnerClass {
     @BeforeMethod
     public boolean setUp() throws InterruptedException {
         // Set up WebDriverManager to automatically download and set up ChromeDriver
-    	System.setProperty("webdriver.http.factory", "jdk-http-client");
+    	//System.setProperty("webdriver.http.factory", "jdk-http-client");
         WebDriverManager.chromedriver().setup();
         RunnerClass.downloadFilePath = AppConfig.downloadFilePath;
 		Map<String, Object> prefs = new HashMap<String, Object>();
